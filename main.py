c3220400b1ee5d9776554a210ef86b2c4180f96d
DmitryZagorodnov <43351927+DmitryZagorodnov@users.noreply.github.com>
2021-12-01 06:05:00 +0300

    #3: Add USD exchange course

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 import json
+from pycbrf.toolbox import ExchangeRates
+from datetime import datetime
 
 
 url = "https://data.messari.io/api/v1/assets/btc/metrics"
@@ -7,3 +9,7 @@ data = urlopen(url).read().decode('utf-8')
 xz = json.loads(data)
 btc_course = xz["data"]["market_data"]["price_usd"]
 print(f"Курс биткоина к американскому доллару: {btc_course}")
+
+rates = ExchangeRates(str(datetime.now().date()))
+usd_course = rates["USD"].value
+print(f"Курс американского доллара к рублю: {usd_course}")
